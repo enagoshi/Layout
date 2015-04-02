5dcdf769658341da5be1f5fe23a996f30eeeefc7
Erin Nagoshi <enagoshi@iolani.org>
2015-04-02 08:46:43 -1000

    Initial Commit

---
+++ Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Layout
+//
+//  Created by enagoshi on 4/2/15.
+//  Copyright (c) 2015 Iolani School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
